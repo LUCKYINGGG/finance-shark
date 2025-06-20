19c463323ede5097fd69fe7c98d22ab2423b7ff4
LUCKYINGGG <39611265+LUCKYINGGG@users.noreply.github.com>
2025-06-19 22:09:33 -0700

    add app.css and index.css

--- frontend/src/components/card/Card.tsx
+++ frontend/src/components/card/Card.tsx
@@ -1,11 +1,22 @@
 import React from "react";
+import "./Card.css";
 
 type Props = {};
 
 const Card = (props: Props) => {
   return (
     <div className="card">
-      <img src="frontend\src\logo.svg" alt="Image" />
+      <img src="\favicon.ico" alt="Test image" />
+      <div className="details">
+        <h2>AAPL</h2>
+        <p>$110</p>
+        <p className="infor">
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aliquam
+          accusamus suscipit iure. Eum similique, quos nesciunt repudiandae
+          architecto doloremque excepturi incidunt repellat magnam, ipsam,
+          dolores magni impedit qui totam dolore.
+        </p>
+      </div>
     </div>
   );
 };
