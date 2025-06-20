02f3b40bbde7d36a769af72f23421f98a2b90d1e
LUCKYINGGG <39611265+LUCKYINGGG@users.noreply.github.com>
2025-06-19 21:32:39 -0700

    start card

---
+++ frontend/src/components/card/Card.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+type Props = {};
+
+const Card = (props: Props) => {
+  return (
+    <div className="card">
+      <img src="frontend\src\logo.svg" alt="Image" />
+    </div>
+  );
+};
+
+export default Card;
